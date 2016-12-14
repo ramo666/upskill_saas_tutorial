e2e47fe211bd51f656a7f753d66c27bf7a68258d
Anze Marinc <anze@marinc.si>
2016-12-14 12:26:21 +0000

    modified environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,7 @@ ActionMailer:Base.smtp_settings = {
   :address        => ENV['MAILGUN_SMTP_SERVER'],
   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         => 'anze.herokuapp.com',
+  :domain         => 'secure-temple-92684.herokuapp.com',
   :authentication => :plain,
 }
 ActionMailer::Base.delivery_method = :smtp
